1ae9cc9498a94c97db8d5b3ff9fdba79cddbfe05
rmcdougal <romanmcdougal01@gmail.com>
2019-12-16 23:39:38 +0000

    Added some validation

--- chkdomains.pl
+++ chkdomains.pl
@@ -4,6 +4,18 @@
 use strict;
 use warnings;
 
-#Validatin cPanel and accounts
+#Validatin cPanel installation and that the server has accounts.
+
+my $domainFile = '/etc/userdomains';
+my $iscPanel = '/usr/local/cpanel/version';
 
 
+if (-e $domainFile && -e $iscPanel ) {
+	
+	print "Files are there";
+
+} else {
+
+	print "This script would be useful on cPanel servers with valid accounts\n";
+
+}
