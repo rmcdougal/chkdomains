1a70918a8e04fd98e06d92ee08b2f1c59c674393
rmcdougal <romanmcdougal01@gmail.com>
2019-12-20 22:43:55 +0000

    Added DNS resolution method

--- chkdomains.pl
+++ chkdomains.pl
@@ -1,8 +1,11 @@
 #!/usr/bin/perl
 
 #Module declaration
+use v5.16;
 use strict;
 use warnings;
+use Data::Dumper;
+use Net::DNS::Resolver;
 use Term::ANSIColor qw(:constants);
 
 #Reset the terminal to default colors when finished.
@@ -25,6 +28,8 @@ if (-e $domainFile && -e $iscPanel ) {
 }
 
 cp_domains();
+resolve_domain();
+
 
 #Pretty printing
 
@@ -55,8 +60,46 @@ sub cp_domains {
 	open(my $fh, $domainFile) or die "Coult not open file '$domainFile'";
 		while (my $row = <$fh>) {
 			chomp $row;
-			print "$row"."\n";
+			my @domain = split /:/, $row;
+			#print "$domain[0]"."\n";
+			#resolve_domain($domain[0]);
+			return ($domain[0]);
 		}		
 }
 
+#Resolving the domain
+
+
+
+sub resolve_domain {
+
+my $name_server1 = '8.8.8.8';
+my $name_server2 = '8.8.4.4';
+
+	#for test
+	my $domain = cp_domains();
+	print Dumper $domain;
+	my $res = Net::DNS::Resolver->new;
+	my $query = $res->search($domain);
+ 	my $result;
+
+		if($query) {
+			
+			foreach my $rr ($query->answer) {
+				
+				if($rr->type eq "A") {
+				
+					$result = $rr->address;
+						
+				}
+				
+				if($result) {
+				
+					say $result;
+
+				} else { say "FAILED";   }
+								
+			}
+		}		
+}
 
